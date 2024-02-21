848ad0fd4c8c946a7c918b06ceff87eb7bcbc2d3
MariaEduardaDeAzevedo <maria.silva@ccc.ufcg.edu.br>
2024-02-21 13:16:45 -0300

    feat: calculating metrics

--- research/download.py
+++ research/download.py
@@ -1,3 +1,8 @@
 import gdown
-gdown.download(id="1tkMXFHsuCCaLRq_P0fbyj1kFA5nrRe2f")
-gdown.download(id="1-B_U48ywDf6wXtmeesks1sOfG8vZtKKt")
+import os
+
+if not os.path.exists("63k-subset-resized.zip"):
+    gdown.download(id="1tkMXFHsuCCaLRq_P0fbyj1kFA5nrRe2f")
+
+if not os.path.exists("test.csv"):
+    gdown.download(id="1-B_U48ywDf6wXtmeesks1sOfG8vZtKKt")
